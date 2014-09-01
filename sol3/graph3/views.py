561b79310864ba7e3e60b49f6a79f4dc567f01e2
michael <michael@michael-G41MT-ES2L>
2014-09-02 09:11:40 +1000

    Main program working again

--- sol3/graph3/views.py
+++ sol3/graph3/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 
 from matplotlib import pylab
 from pylab import *
+import PIL
+import PIL.Image
+import StringIO
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+ 
 
-def graph():
+def graph(request):
     x = [1,2,3,4,5,6]
     y = [3,5,6,4,4,4]
     plot(x,y, linewidth=2)
@@ -12,4 +18,13 @@ def graph():
     ylabel('y axis')
     title('sample graph')
     grid(True)
+    
+    buffer = StringIO.StringIO()
+    canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    graphIMG = PIL.Image.fromstring("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    graphIMG.save(buffer,"PNG")
+    pylab.close()
+    
+    return HttpResponse(buffer.getvalue(), mimetype = "image/png")
     
--- sol3/sol3/urls.py
+++ sol3/sol3/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from graph3 import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^graph/$', views.graph, name="graph"),
+
 )
